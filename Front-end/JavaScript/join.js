648dd548315ef9aadd3d3cc93d7765edfe2614ed
Galaxy <js_park95@naver.com>
2023-08-21 12:09:10 +0900

    join 필수정보 입력해야 가입버튼 활성화되게 구현

--- Front-end/JavaScript/join.js
+++ Front-end/JavaScript/join.js
@@ -3,7 +3,10 @@ const loginInfo = form.querySelector('section.login-info')
 const userInfo = form.querySelector('section.user-info')
 const joinId = loginInfo.querySelector('input#join-id')
 const joinPassword = loginInfo.querySelector('input#join-password')
+const joinEmail = loginInfo.querySelector('input#join-email')
+const userName = userInfo.querySelector('input#user-name')
 const visibility = loginInfo.querySelector('div.visibility')
+const submit = form.querySelector('input[type="submit"]')
 
 visibility.addEventListener('click', function(e){
     const on = visibility.querySelector('.on')
@@ -16,5 +19,13 @@ visibility.addEventListener('click', function(e){
         joinPassword.setAttribute('type', 'text')
     }else if(off.className.includes('active')){
         joinPassword.setAttribute('type', 'password')
+    }
+})
+
+form.addEventListener('input', function(e){
+    if(joinId.value !== "" && joinPassword.value !== "" &&
+    joinEmail.value !== "" && userName.value !== ""){
+        submit.classList.add('active')
+        submit.setAttribute('disabled', false)
     }
 })
