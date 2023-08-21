ad40e8326309eba40f4a7f239551edcbef11d369
Galaxy <js_park95@naver.com>
2023-08-21 18:13:52 +0900

    join 회원가입 구현중

--- Front-end/JavaScript/join.js
+++ Front-end/JavaScript/join.js
@@ -1,12 +1,12 @@
 const form = document.querySelector('form')
 const loginInfo = form.querySelector('section.login-info')
 const userInfo = form.querySelector('section.user-info')
-const joinId = loginInfo.querySelector('input#join-id')
-const joinPassword = loginInfo.querySelector('input#join-password')
-const joinEmail = loginInfo.querySelector('input#join-email')
-const userName = userInfo.querySelector('input#user-name')
+const joinId = loginInfo.querySelector('input#userId')
+const joinPassword = loginInfo.querySelector('input#password')
+const joinEmail = loginInfo.querySelector('input#email')
+const userName = userInfo.querySelector('input#user_name')
 const visibility = loginInfo.querySelector('div.visibility')
-const submit = form.querySelector('input[type="submit"]')
+const submit = form.querySelector('div.submit button')
 
 visibility.addEventListener('click', function(e){
     const on = visibility.querySelector('.on')
@@ -23,9 +23,32 @@ visibility.addEventListener('click', function(e){
 })
 
 form.addEventListener('input', function(e){
-    if(joinId.value !== "" && joinPassword.value !== "" &&
-    joinEmail.value !== "" && userName.value !== ""){
+    
+    if(![joinId.value.trim(), joinPassword.value.trim(), joinEmail.value.trim(),
+        userName.value.trim()].includes("")){
         submit.classList.add('active')
-        submit.setAttribute('disabled', false)
+        submit.removeAttribute('disabled')
+
+        
+
+    }else{
+        submit.setAttribute('disabled', true)
     }
+
+})
+
+submit.addEventListener('click', async function(e){
+
+    const res = await fetch('http://localhost:5000/api/users/join', {
+        method: 'POST',
+        body: JSON.stringify({
+            userId: joinId.value,
+            password: joinPassword.value,
+            email: joinEmail.value,
+            user_name: userName.value
+        })
+    })
+
+    // const response = await res.json()
+    // console.log(response)
 })
